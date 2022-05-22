518ae3ff3de2117e13e97551cbce94cc648ae767
晓晓 墨 <1924645279@qq.com>
2022-05-22 22:37:55 +0800

    a

--- test.py
+++ test.py
@@ -3,5 +3,9 @@ url = 'http://localhost:8089/api/tr-run/'
 img1_file = {
     'file': open('1.jpg', 'rb')
 }
-res = requests.post(url=url, data={'compress': 0}, files=img1_file).json()
-print(res)
+res = requests.post(url=url, data={'compress': 0}, files=img1_file).json().get('data',{})
+temp = res.get('raw_out', [])
+Str = ""
+for List in temp:
+    Str += List[1]
+print(Str)
